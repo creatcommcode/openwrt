38235dc6423beeec443e6dcb05017fe9a803c0da
amykkoo <amykkoo@hotmail.com>
2020-06-17 06:29:57 +0000

    ar71xx: add support for Creatcomm TB2I
    * QCA QCA9531
    * 64 MB of RAM
    * 8 MB of SPI NOR flash (mx25l6405d)
    * 1x button (reset)
    * 3x Fast ethernet
    
    The sysupgrade image can be installed directly on flash using u-boot:
      tftpboot 0x80060000 openwrt-ar71xx-generic-cc_tb2i-squashfs-sysupgrade.bin
      erase 0x9f050000 +$filesize
      cp.b $fileaddr 0x9f050000 $filesize

--- target/linux/ar71xx/base-files/lib/upgrade/platform.sh
+++ target/linux/ar71xx/base-files/lib/upgrade/platform.sh
@@ -324,7 +324,8 @@ platform_check_image() {
 	wzr-hp-g300nh|\
 	wzr-hp-g300nh2|\
 	wzr-hp-g450h|\
-	xd3200)
+	xd3200|\
+	tb2i)
 		[ "$magic" != "2705" ] && {
 			echo "Invalid image type."
 			return 1
